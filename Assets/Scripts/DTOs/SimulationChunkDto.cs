878edbd14ea8d599ca73c08e1fbc352d0a46ff68
Aleksander Blaszkiewicz <aleksander.blaszkie@ihsmarkit.com>
2022-12-11 16:08:00 +0100

    Add crossroad and roundabout scenes

--- Assets/Scripts/DTOs/SimulationChunkDto.cs
+++ Assets/Scripts/DTOs/SimulationChunkDto.cs
@@ -6,8 +6,8 @@ namespace Assets.Scripts.DTOs
     [Serializable]
     public class SimulationChunkDto
     {
-        public string settingsHash = "test";
-        public string mapHash = "roundabout";
+        public string settingsHash = "random";
+        public string mapHash = "crossroad";
         public string runId = "1";
         public List<FrameDto> frames = new List<FrameDto>();
     }
