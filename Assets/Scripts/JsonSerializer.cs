0818090989c2afa3e296badede08ea8fee079407
Aleksander Błaszkiewicz <54682207+ablaszkiewicz@users.noreply.github.com>
2022-12-17 13:41:42 +0100

    Finish pieckiMigowo fragment

--- Assets/Scripts/JsonSerializer.cs
+++ Assets/Scripts/JsonSerializer.cs
@@ -21,7 +21,7 @@ namespace Assets.Scripts
 
         private bool simulationOver = false;
         private int rateLimitterCounter = 0;
-        private int rateLimitter = 20;
+        private int rateLimitter = 40;
         private MapDto mapDto;
 
         [SerializeField]
